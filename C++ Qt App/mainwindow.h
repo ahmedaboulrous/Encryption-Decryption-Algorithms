dc984d048a35946495cb4a06057b2f2eb574b336
Ahmed Aboulrous <aminaboulrous@gmail.com>
2018-11-11 04:40:06 +0200

    first commit

---
+++ C++ Qt App/mainwindow.h
@@ -0,0 +1,67 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <QMessageBox>
+#include <iostream>
+
+struct Position {
+    int x;
+    int y;
+};
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_encrypt_clicked();
+    void on_pushButton_decrypt_clicked();
+
+    void on_actionReset_Fields_triggered();
+
+    void on_actionClear_Plain_Text_triggered();
+
+    void on_actionClear_Encrypted_Text_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionAbout_Qt_triggered();
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+    QString ewCaesarCipher(QString plainText, int key);
+    QString dwCaesarCipher(QString encryptedText, int key);
+
+    QString ewPlayfair(QString plainText, QString secret);
+    QString dwPlayfair(QString encryptedText, QString secret);
+
+    char pfMatrix[5][5];
+    bool inSameRow(Position p1, Position p2);
+    bool inSameColumn(Position p1, Position p2);
+    void normalizePFString(std::string &str);
+    void normalizePFSecret(std::string &secret);
+    void populatePFMatrix(std::string secret);
+    std::string encryptPFString(std::string plainStdText);
+    std::string decryptPFString(std::string encryptedStdText);
+    Position findPositionByChar(char ch);
+    char findCharByPosition(Position p);
+
+};
+
+
+
+#endif // MAINWINDOW_H
